55559b1fdb66fffcb93cd9ccc68eaff6d423c264
Oliver Cederborg <3323050@rts-365.dk>
2021-05-25 12:36:29 +0200

    feat: add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { page } from '$app/stores'
+</script>
+
+<header class="bg-gray-100 shadow-lg">
+	<div class="flex container items-center justify-between">
+		<h1 class="font-bold text-2xl">INDIE</h1>
+
+		<nav class="space-x-4">
+			<a
+				href="."
+				class="p-4 text-lg inline-flex items-center {$page.path == '/' &&
+					'border-b-2 border-yellow-500'}">Todos</a
+			>
+			<a
+				href="chat"
+				class="p-4 text-lg inline-flex items-center {$page.path ==
+					'/chat' && 'border-b-2 border-yellow-500'}">Chat</a
+			>
+		</nav>
+	</div>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,24 +1,8 @@
 <script>
 	import '../tailwind.css'
-	import { page } from '$app/stores'
+	import Header from '$components/Header.svelte'
 </script>
 
-<header class="bg-gray-100 shadow-lg">
-	<div class="flex container items-center justify-between">
-		<h1 class="font-bold text-2xl">INDIE</h1>
+<Header />
 
-		<nav class="space-x-4">
-			<a
-				href="."
-				class="p-4 text-lg inline-flex items-center {$page.path == '/' &&
-					'border-b-2 border-yellow-500'}">Todos</a
-			>
-			<a
-				href="test"
-				class="p-4 text-lg inline-flex items-center {$page.path ==
-					'/test' && 'border-b-2 border-yellow-500'}">Test</a
-			>
-		</nav>
-	</div>
-</header>
 <slot />
