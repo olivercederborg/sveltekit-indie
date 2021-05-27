016cd4c6ee72cd94d34fcea042c6e0da95a94e2c
Oliver Cederborg <hi@olivercederborg.com>
2021-05-28 00:29:15 +0200

    style(header): change to fixed position + minor styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,21 +2,21 @@
 	import { page } from '$app/stores'
 </script>
 
-<header class="bg-gray-100 shadow-lg">
-	<div class="flex container items-center justify-between">
-		<h1 class="font-bold text-2xl">INDIE</h1>
+<header
+	class="bg-gray-100 shadow-lg fixed top-0 right-0 left-0 z-10 flex px-4 items-center justify-between"
+>
+	<h1 class="font-bold text-2xl">INDIE</h1>
 
-		<nav class="space-x-4">
-			<a
-				href="."
-				class="p-4 text-lg inline-flex items-center {$page.path == '/' &&
-					'border-b-2 border-yellow-500'}">Todos</a
-			>
-			<a
-				href="chat"
-				class="p-4 text-lg inline-flex items-center {$page.path ==
-					'/chat' && 'border-b-2 border-yellow-500'}">Chat</a
-			>
-		</nav>
-	</div>
+	<nav class="space-x-4">
+		<a
+			href="."
+			class="p-4 text-lg inline-flex items-center {$page.path == '/' &&
+				'border-b-2 border-yellow-500'}">Todos</a
+		>
+		<a
+			href="chat"
+			class="p-4 text-lg inline-flex items-center {$page.path == '/chat' &&
+				'border-b-2 border-yellow-500'}">Chat</a
+		>
+	</nav>
 </header>
